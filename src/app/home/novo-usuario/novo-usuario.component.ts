a5795bf0bbda07f85e3ec134a0ce6a4d018acbdd
Anderson Silva <andssilva.23@gmail.com>
2022-08-28 00:19:39 -0400

    feat: :rocket: cria o formulário reativo

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor() { }
+  novoUsuarioForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) { }
 
   ngOnInit(): void {
+    this.criaFormBuild();
+  }
+
+  criaFormBuild() {
+    this.novoUsuarioForm = this.fb.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: ['']
+    })
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario)
   }
 
 }
