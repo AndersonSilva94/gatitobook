44a5484a6d47eae2d7030a5c1ffe856d564dacb1
Anderson Silva <andssilva.23@gmail.com>
2022-08-31 23:55:12 -0400

    feat: :rocket: refatoração da api para variáveis de ambiente

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http'
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { UsuarioService } from './usuario/usuario.service';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +17,7 @@ export class AutenticacaoService {
   ) { }
 
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
-    return this.http.post('http://localhost:3000/user/login', {
+    return this.http.post(`${API}/user/login`, {
       userName: usuario,
       password: senha
     }, {
