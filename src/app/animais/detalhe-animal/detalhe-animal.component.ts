01e69deddb5190ba5ad467abb96dbc5713135eb5
Anderson Silva <andssilva.23@gmail.com>
2022-09-04 13:11:55 -0400

    feat: :rocket: implementa os métodos no componente de detalhes do animal

--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Animal } from '../animais';
 import { AnimaisService } from '../animais.service';
@@ -16,12 +16,31 @@ export class DetalheAnimalComponent implements OnInit {
 
   constructor(
     private animaisService: AnimaisService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.animalId = this.activatedRoute.snapshot.params['id']
     this.animal$ = this.animaisService.buscaAnimalPorId(this.animalId)
+  }
+
+  curtir() {
+    this.animaisService.curtirAnimal(this.animalId)
+      .subscribe(
+        (curtida) => {
+          if(curtida) this.animal$ = this.animaisService.buscaAnimalPorId(this.animalId)
+        },
+        (error) => console.error(error)
+      )
+  }
+
+  excluir() {
+    this.animaisService.excluiAnimal(this.animalId)
+      .subscribe(
+        () => this.router.navigate(['/animais/']),
+        (error) => console.error(error)
+      )
   }
 
 }
