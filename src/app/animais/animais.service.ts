2b9cb5b67d14189407d0324ddd050f492b05bb6b
Anderson Silva <andssilva.23@gmail.com>
2022-09-04 00:38:50 -0400

    feat: :rocket: criando um serviço dinâmico usando ActivatedRoute

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../autenticacao/token.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
 
@@ -21,6 +21,14 @@ export class AnimaisService {
     const token = this.tokenService.retornaToken();
     const headers = new HttpHeaders().append('x-access-token', token);
     return this.http.get<Animais>(`${API}/${nomeUsuario}/photos`, {
+      headers
+    })
+  };
+
+  buscaAnimalPorId(id: number): Observable<Animal> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animal>(`${API}/photos/${id}`, {
       headers
     })
   }
