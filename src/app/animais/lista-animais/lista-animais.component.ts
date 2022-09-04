3333a5c5b160e36b0f76493fb93d0262727372fa
Anderson Silva <andssilva.23@gmail.com>
2022-09-04 14:36:22 -0400

    feat: :rocket: criando a guarda Resolver

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
-import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { Animais } from '../animais';
-import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
@@ -11,21 +10,16 @@ import { AnimaisService } from '../animais.service';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais$!: Observable<Animais>;
+  animais!: Animais;
 
   constructor(
-    private usuarioService: UsuarioService,
-    private animaisService: AnimaisService
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.animais$ = this.usuarioService.retornaUsuario()
-      .pipe(
-        switchMap((usuario) => {
-          const userName = usuario.name ?? '';
-          return this.animaisService.listaDoUsuario(userName)
-        })
-      );
+    this.activatedRoute.params.subscribe(params => {
+      this.animais = this.activatedRoute.snapshot.data['animais']
+    })
   }
 
 }
